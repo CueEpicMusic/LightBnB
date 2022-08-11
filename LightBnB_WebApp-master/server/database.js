898e53b597eaff10253490de215b9708ce5803b1
CueEpicMusic <chiatunglo@hotmail.com>
2022-08-11 00:02:07 +0000

    refactor addProperty to dynamically add a property to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -160,7 +160,24 @@ exports.getAllProperties = getAllProperties;
 const addProperty = function (property) {
   return pool
     .query(
-      `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)RETURNING *;`,
+      `INSERT INTO properties 
+      (
+        owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        street,
+        city,
+        province,
+        post_code,
+        country,
+        parking_spaces,
+        number_of_bathrooms,
+        number_of_bedrooms
+      )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)RETURNING *;`,
       [
         property.owner_id,
         property.title,
@@ -168,14 +185,14 @@ const addProperty = function (property) {
         property.thumbnail_photo_url,
         property.cover_photo_url,
         property.cost_per_night,
-        property.parking_spaces,
-        property.number_of_bathrooms,
-        property.number_of_bedrooms,
-        property.country,
         property.street,
         property.city,
         property.province,
         property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
       ]
     )
     .then((result) => {
